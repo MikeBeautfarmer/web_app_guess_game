02e285d381151fdbeecbe4f5e8a7a9a4855c8a1e
Michael Schönbauer <michaelschoenbauer@gmx.at>
2020-10-27 17:31:55 +0100

    added change password and edit profile etc

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def index():
     session_token = request.cookies.get("session_token")
 
     if session_token:
-        user = db.query(User).filter_by(session_token=session_token).first()
+        user = db.query(User).filter_by(session_token=session_token, deleted=False).first()
     else:
         user = None
 
@@ -51,10 +51,9 @@ def login():
     db.add(user)
     db.commit()
 
-    response = make_response(redirect(url_for("game")))
+    response = make_response(redirect(url_for("index")))
     response.set_cookie("session_token", session_token, httponly=True, samesite="Strict")
     return response
-    # response = make_response(redirect(url_for("")))
 
 
 @app.route("/result", methods=["POST"])
@@ -67,7 +66,7 @@ def result():
 
     session_token = request.cookies.get("session_token")
 
-    user = db.query(User).filter_by(session_token=session_token).first()
+    user = db.query(User).filter_by(session_token=session_token, deleted=False).first()
 
     if rate == user.geheime_zahl:  # Wenn geheime Zahl erraten wird!
         nachricht = "Korrekt! Die geheime Zahl ist {0}".format(str(user.geheime_zahl))  # Nachricht Ausgabe
@@ -84,6 +83,81 @@ def result():
     elif rate > user.geheime_zahl:  # Wenn Ratezahl größer als geheime Zahl
         nachricht = "Falsch! . . .   Versuche eine niedrigere Zahl"  # Nachricht Ausgabe
         return render_template("result.html", nachricht=nachricht)  # Nachricht in result.html
+
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    session_token = request.cookies.get("session_token")
+    user = db.query(User).filter_by(session_token=session_token, deleted=False).first()
+    if user:
+        return render_template("profile.html", user=user)
+    else:
+        return redirect(url_for("index2"))
+
+
+@app.route("/profile/edit", methods=["GET", "POST"])
+def profile_edit():
+    session_token = request.cookies.get("session_token")
+    user = db.query(User).filter_by(session_token=session_token, deleted=False).first()
+
+    if request.method == "GET":
+        if user:
+            return render_template("profile_edit.html", user=user)
+        else:
+            return redirect(url_for("login"))
+    elif request.method == "POST":
+        name = request.form.get("profile-name")
+        email = request.form.get("profile-email")
+        old_password = request.form.get("old-password")
+        new_password = request.form.get("new-password")
+        if old_password and new_password:
+            hashed_old_password = hashlib.sha256(old_password.encode()).hexdigest()
+            hashed_new_password = hashlib.sha256(new_password.encode()).hexdigest()
+            if hashed_old_password == user.password:
+                user.password = hashed_new_password
+            else:
+                return "Falsches Passwort! Geh zurück und versuche es noch einmal."
+
+        user.name = name
+        user.email = email
+
+        db.add(user)
+        db.commit()
+
+        return redirect(url_for("profile"))
+
+
+@app.route("/profile/delete", methods=["GET", "POST"])
+def profile_delete():
+    session_token = request.cookies.get("session_token")
+    user = db.query(User).filter_by(session_token=session_token, deleted=False).first()
+
+    if request.method == "GET":
+        if user:
+            return render_template("profile_delete.html", user=user)
+        else:
+            return redirect(url_for("index"))
+    elif request.method == "POST":
+
+        user.deleted = True
+        db.delete(user)
+        db.commit()
+
+        return redirect(url_for("index"))
+
+
+@app.route("/users", methods=["GET"])
+def all_users():
+    users = db.query(User).filter_by(deleted=False).all()
+
+    return render_template("users.html", users=users)
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def user_details(user_id):
+    user = db.query(User).get(int(user_id))
+
+    return render_template("user_details.html", user=user)
 
 
 if __name__ == '__main__':
